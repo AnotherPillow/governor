64bc417b8ec99d2ff0f28be7ff907f839c9f0b55
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2024-02-29 17:48:07 +0000

    Revert "Automatically execute XNBZola for linked XNB mods (#43)"
    This reverts commit e67e80ce8501f3b599f83b1388a1a18089844a6c.

--- src/config.py
+++ src/config.py
@@ -16,7 +16,6 @@ ASSETS_PATH = "./assets"
 FONTS_PATH = "./fonts"
 TMP_PATH = "./private/tmp"
 CMD_PREFIX = cfg['command_prefix']
-NEXUS_API_KEY = cfg['nexus_key']
 
 ADMIN_ACCESS = cfg['roles']['admin_access']
 MODDER_ROLE = cfg['roles']['modder']
